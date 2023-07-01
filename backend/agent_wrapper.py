67eeb8068d278ce14004a25203f7f0b52b3997cf
xovale <xo.ovale@gmail.com>
2023-07-01 13:13:15 -0400

    clase agente

--- backend/agent_wrapper.py
+++ backend/agent_wrapper.py
@@ -1,3 +1,18 @@
+import judini
+from judini.agent import Agent
+
+
 class Agent:
 
+    def __init__(self, api_key, agent_id_classifier, agent_id_response):
+        self.agent_classifier = Agent(api_key,agent_id_classifier)
+        self.agent_response = Agent(api_key,agent_id_response)
+
+    def Classifier(self, prompt):
+        response = self.agent_classifier.completion(prompt=prompt, stream=False)
+        return response
     
+    def Response(self, prompt):
+        response = self.agent_response.completion(prompt=prompt, stream=False)
+        return response
+
